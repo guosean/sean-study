fb23233ce3689334dde1d02ea3e379f2b9d94439
guosean <sean4478@gmail.com>
2014-05-30 17:16:13 +0800

    classloader

---
+++ src/main/java/com/sean/jdk/extend/TestPro.java
@@ -0,0 +1,24 @@
+package com.sean.jdk.extend;
+
+/**
+ * <p> Copyright 200 by Asiainfo-Linkage Software corporation
+ * <p>All rights reserved.
+ * <p>版权所有：亚信联创
+ * <p>未经本公司许可，不得以任何方式复制或使用本程序任何部分，
+ * <p>侵权者将受到法律追究。
+ * @PURPOSE:  TODO
+ * @DESCRIPTION: TODO
+ * @AUTHOR: guozb
+ * @DATE: 2014-5-30
+ * @VERSION PSMA
+ * @SINCE 1.0
+ * @HISTORY: 2.0
+ */
+public class TestPro {
+	
+	public static void main(String[] args) {
+		Parent p = new Parent();
+
+	}
+
+}
--- src/main/java/com/sean/jdk/loader/ClassLoaderTest.java
+++ src/main/java/com/sean/jdk/loader/ClassLoaderTest.java
@@ -2,12 +2,50 @@ package com.sean.jdk.loader;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
+
+import org.junit.Test;
 /**
  * 类加载器
  * @author sean
  *
  */
 public class ClassLoaderTest {
+	/**
+	 * SystemClassLoader:  sun.misc.Launcher$AppClassLoader
+	 * ExtClassLoader:     sun.misc.Launcher$ExtClassLoader
+	 */
+    @Test
+	public void testParent(){
+		ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+		while(classLoader!=null){
+			System.out.println(classLoader.getClass());
+			classLoader = classLoader.getParent();
+		}
+	}
+    /**
+     * 测试两个类加载器加载同一个类
+     */
+    @Test
+    public void testTwoClassLoader(){
+    	String classPath = "D:\\myworkspace\\sean-study\\target\\classes";
+    	FileSystemClassLoader fsc1 = new FileSystemClassLoader(classPath);
+    	FileSystemClassLoader fsc2 = new FileSystemClassLoader(classPath);
+    	String className = "com.sean.jdk.loader.Sample";
+        try{
+        	Class<?> c1 = fsc1.findClass(className);
+        	System.out.println(c1.getClassLoader());
+        	Object o1 = c1.newInstance();
+        	Class<?> c2 = fsc2.findClass(className);
+        	System.out.println(c1.getClassLoader());
+        	Object o2 = c2.newInstance();
+        	Method md = c1.getMethod("setSample", Object.class);
+        	md.invoke(o1, o2);
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+    	
+    }
 	
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
 	  	ClassLoader loader = new ClassLoader() {
