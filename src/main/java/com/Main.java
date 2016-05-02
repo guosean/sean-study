424f139f5d7acfb06e5b15b4c659910fa869483b
郭振宾 <sean4478@gmail.com>
2016-05-02 11:34:44 +0800

    dg

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,27 +1,15 @@
 package com;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-import com.google.common.collect.Lists;
-
 /**
  * @AUTHOR: guozb
  * @DATE: 2014-6-23
  */
 public class Main {
-  public static void main(String[] args) {
-	List<String> list = new CopyOnWriteArrayList<String>();
-	for (int i = 0; i < 5; i++) {
-		list.add(String.valueOf(i));
-	}
-	Iterator<String> it = list.iterator();
-	while(it.hasNext()){
-		it.next();
-//		it.remove();
-		list.add("test");
-	}
-	System.out.println(list);
+
+    public static void main(String[] args) {
+        String sd="d";
+        System.out.println(sd);
+        System.out.println("test");
+      System.out.println("hello world");
 }
 }
